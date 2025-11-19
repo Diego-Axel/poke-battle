11452334c7dae885b7a7ca4d2eeafa0daaa9ecba
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-11-19 11:25:07 -0300

    feat: ler o ?player=X da URL.

--- src/app/battle/[id]/page.tsx
+++ src/app/battle/[id]/page.tsx
@@ -5,16 +5,21 @@ interface BattlePageProps {
   params: {
     id: string;
   };
+  // O Next.js injeta os query params aqui automaticamente
+  searchParams: {
+    player?: string;
+  };
 }
 
-export default async function BattlePage({ params }: BattlePageProps) {
-  // 1. Busca os dados no servidor (Server-side Data Fetching)
-  // Isso acontece ANTES da página ser enviada para o navegador
-  const [playerPokemon, opponentPokemon] = await fetchBattlePokemons();
+export default async function BattlePage({ params, searchParams }: BattlePageProps) {
+  // Converte a string "25" para número 25 ou undefined
+  const playerId = searchParams.player ? Number(searchParams.player) : undefined;
+
+  // Passamos o ID escolhido para a função
+  const [playerPokemon, opponentPokemon] = await fetchBattlePokemons(playerId);
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-[url('/grid.svg')] bg-center">
-       {/* Passamos os dados prontos para o componente interativo */}
+    <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-[url('/grid.svg')] bg-center pt-20">
        <BattleBoard 
           playerPokemon={playerPokemon} 
           opponentPokemon={opponentPokemon} 
